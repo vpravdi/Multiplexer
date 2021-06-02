197cd7e90677a7b12ca73ff444fe7beb3acd0134
vpravdi <dineshpraveen91@gmail.com>
2021-06-02 19:15:15 +0530

    building on serving form

--- nethandler.go/nethandler.go
+++ nethandler.go/nethandler.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 type trial int
 
-func (m trial) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(w, "Request handled")
-	fmt.Println(w, "Request")
+func (m trial) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+
 }
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
 func main() {
 	var d trial
 	http.ListenAndServe(":8080", d)

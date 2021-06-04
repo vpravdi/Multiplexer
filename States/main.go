42ad062945becc393d657cfa2269e4723aa09cf2
vpravdi <dineshpraveen91@gmail.com>
2021-06-04 21:32:00 +0530

    upload func

--- States/main.go
+++ States/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -12,14 +14,38 @@ func main() {
 }
 
 func a(res http.ResponseWriter, req *http.Request) {
-	a := req.FormValue("a")
+	var a string
+	fmt.Println(req.Method)
+
 	//io.WriteString(res, "dog:\t"+a+"\n")
 	//io.WriteString(res, "cat:\t"+b+"\n")
+	if req.Method == http.MethodPost {
+		//open the file
+		f, h, err := req.FormFile("q")
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		//FYI
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr:", err)
+
+		//read
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		a = string(bs)
+	}
+
 	res.Header().Set("Content-Type", "text/html; charset=utf-8")
 	io.WriteString(res, `
-	<form method="post">
-		<input type="text" name="a">
+	<form method="post" enctype="multipart/form-data">
+		<input type="file" name="q">
 		<input type="submit">
 	</form>
 	<br>`+a)
+
 }

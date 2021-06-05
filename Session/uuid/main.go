11812a70adf99a24a7f04b618ba456a81e8b2871
vpravdi <dineshpraveen91@gmail.com>
2021-06-05 19:59:55 +0530

    uuid generation

---
+++ Session/uuid/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		id := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:     "session",
+			Value:    id.string(),
+			HttpOnly: true,
+		}
+		http.SetCookie(res, cookie)
+	}
+	fmt.Println(cookie)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Multiplexer
 
 go 1.16
+
+require github.com/satori/go.uuid v1.2.0 // indirect

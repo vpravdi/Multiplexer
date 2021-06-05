97062f9a662642f3a7dfcaaa3d3e23841db90d40
vpravdi <dineshpraveen91@gmail.com>
2021-06-06 00:12:37 +0530

    uuid successful

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module Multiplexer
 
 go 1.16
 
-require github.com/satori/go.uuid v1.2.0 // indirect
+require (
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+
+)
--- nethandler.go/main.go
+++ nethandler.go/main.go
@@ -19,14 +19,14 @@ func (m trial) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "<h1>Executing</h1>")
 
 	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
-
+ 
 }
 
 var tpl *template.Template
 
 func init() {
 	tpl = template.Must(template.ParseFiles("index.gohtml"))
-}
+} 
 func main() {
 	var d trial
 	http.ListenAndServe(":8080", d)

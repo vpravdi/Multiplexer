12f32814b916e225e0fb4261635b8845aade9c5a
vpravdi <dineshpraveen91@gmail.com>
2021-06-07 14:37:20 +0530

    added appendValue

--- blog/main.go
+++ blog/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -22,6 +23,7 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
+	c = appendValue(w, c)
 	tpl.ExecuteTemplate(w, "index.gohtml", c.Value)
 	fmt.Fprintln(w, c.Value)
 }
@@ -37,4 +39,23 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 		http.SetCookie(w, c)
 	}
 	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	p1, p2, p3 := "disney.jpg", "beach.jpg", "hollywood.jpg"
+
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+
 }
